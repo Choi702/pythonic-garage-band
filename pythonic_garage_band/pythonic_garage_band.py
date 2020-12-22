f11864089f957c55c1a8d323b53aeedadf98738c
Choi702 <jaeychoi1@gmail.com>
2020-12-22 12:32:06 -0800

    14 passing

--- pythonic_garage_band/pythonic_garage_band.py
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -1,18 +1,63 @@
 class Band:
-     def __init__(self, name, members=None):
+   instances = []
+   def __init__(self, name, members=None):
         self.name = name
         self.members = members
+        Band.instances.append(self)
      
-       
-     def __str__(self):
+   def __str__(self):
         return f"The band {self.name}"
 
-     def __repr__(self):
+   def __repr__(self):
         return f"Band instance. name={self.name}, members={self.members}"
     
-  
+   def play_solos(self):
+         solo = []
+         for member in self.members:
+            solo.append(member.play_solos()) 
+         return solo
 
-# if__name__ == "__main__":
-#   band = Band('Joe')
+   @classmethod
+   def to_list(cls):
+      return cls.instances
+
+#parent class
+class Musician:
+   def __init__(self, name, title, instrument):
+      self.name = name
+      self.title = title
+      self.instrument = instrument
+  
+   def __str__(self):
+      return f"My name is {self.name} and I play {self.instrument}"
+   
+   def __repr__(self):
+      return f"{self.title} instance. Name ={self.name}"
+
+   def get_instrument(self):
+      return f"{self.instrument}"
+
+class Guitarist(Musician):
+   def __init__(self, name):
+      super().__init__(name, 'Guitarist', 'guitar')
+   
+   def play_solo(self):
+      return 'face melting guitar solo'
+
+class Bassist(Musician):
+   def __init__(self, name):
+      super().__init__(name, 'Bassist', 'bass')
+   
+   def play_solo(self):
+      return 'bom bom buh bom'
+
+
+class Drummer(Musician):
+   def __init__(self, name):
+      super().__init__(name, 'Drummer', 'drums')
+   
+   def play_solo(self):
+      return 'rattle boom crash'
+
 
 
